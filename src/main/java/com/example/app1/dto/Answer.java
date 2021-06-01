a638d4ed59ee4a3d3ab3363dbb51677be288d9ac
Iulia Chebykina <julia.chebykina.13@gmail.com>
2021-06-01 14:46:56 +0500

    Chebykina Iulia

--- src/main/java/com/example/app1/dto/Answer.java
+++ src/main/java/com/example/app1/dto/Answer.java
@@ -1,11 +1,16 @@
 package com.example.app1.dto;
 
 
+
+import com.fasterxml.jackson.annotation.JsonFilter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+
 @Data
 @AllArgsConstructor
+@JsonFilter("myFilter")
 public class Answer {
     private String id;
     private State state;
--- src/main/java/com/example/app1/service/ConsumerService.java
+++ src/main/java/com/example/app1/service/ConsumerService.java
@@ -4,6 +4,9 @@ import com.example.app1.dto.Answer;
 import com.example.app1.dto.ModifiedAnswer;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+import lombok.SneakyThrows;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
@@ -21,14 +24,27 @@ public class ConsumerService {
         return this::listen;
     }
 
+
     public void listen(Answer answer) {
         try {
             log.info("Получено " + mapper.writeValueAsString(answer));
         } catch (JsonProcessingException e) {
             log.error(e.getMessage());
         }
-        var modifiedAnswer = new ModifiedAnswer(answer.getId(), answer.getState(), answer.getTime());
+        var modifiedAnswer = new ModifiedAnswer(answer.getId(), answer.getState(), answer.getTime());//не уверена, как лучше делать
         System.out.println(modifiedAnswer);
+        /*SimpleFilterProvider filterProvider = new SimpleFilterProvider().setFailOnUnknownId(false);
+        filterProvider.addFilter("myFilter",
+                SimpleBeanPropertyFilter.serializeAllExcept("message"));
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.setFilterProvider(filterProvider);
+        try {
+            var str = mapper.writeValueAsString(answer);
+            System.out.println(str);
+        } catch (JsonProcessingException e) {
+            log.error(e.getMessage());
+        }*/
     }
 
 }
